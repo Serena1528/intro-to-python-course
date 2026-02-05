b079936e7a0478984540aa53c422f0fc9e45855a
Serena <serenathurlow55@gmail.com>
2026-02-05 11:40:00 -0600

    big changes huge and big

--- PyCharmMiscProject/Week1/lecture_code_1_SerenaT.py
+++ PyCharmMiscProject/Week1/lecture_code_1_SerenaT.py
@@ -3,12 +3,20 @@ import webbrowser
 #attempts this first
 try:
     seconds = int(input('Enter the number of seconds: '))
+    mode = input('Do you want to play a video when done? ')
+    url = 'https://www.youtube.com/watch?v=UG3VcCAlUgE'
 
     if seconds < 0:
         print('Please enter a positive number!')
     else:
         time.sleep(seconds)
-        print('Time is up!')
+        if mode.lower() == 'yes':
+            webbrowser.open_new_tab(url)
+        elif mode.lower() == 'no':
+            print('Time is up!')
+        else:
+            print('Plese enter a yes or a no! ')
+
 
 
 #if there is a ValueError then it prints this
